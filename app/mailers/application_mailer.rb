5634cb3ec5df0fe0364c83ee0cd4fbbe62fac830
Uchenna Mba <urchymanny@gmail.com>
2023-01-08 01:08:14 +0100

    feat(emails): test dynamic emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,41 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'uche@jazzysburger.com'
+  default from: 'noreply@jazzysburger.com'
   layout 'mailer'
+
+  EMAIL = {
+    receipts: {
+      user_name: ENV['RECEIPTS_EMAIL'],
+      password: ENV['RECEIPTS_PASSWORD']
+    },
+    notify: {
+      user_name: ENV['NOTIFY_EMAIL'],
+      password: ENV['NOTIFY_PASSWORD']
+    },
+    web: {
+      user_name: ENV['WEB_EMAIL'],
+      password: ENV['WEB_PASSWORD']
+    },
+    noreply: {
+      user_name: ENV['NOREPLY_EMAIL'],
+      password: ENV['NOREPLY_PASSWORD']
+    }
+  }
+
+  private
+
+  def set_receipt_email
+    @delivery_options = EMAIL[:receipts]
+  end
+
+  def notify_email
+    @delivery_options = EMAIL[:notify]
+  end
+
+  def web_email
+    @delivery_options = EMAIL[:web]
+  end
+
+  def noreply_email
+    @delivery_options = EMAIL[:noreply]
+  end
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,9 @@
 class OrderMailer < ApplicationMailer
+  before_action :set_receipt_email, only: [:receipt_email]
 
   def receipt_email
     @order = Order.find_by(reference: params[:reference])
 
-    mail(to: 'urchman0000@gmail.com', subject: "You got a new order!")
+    mail(to: 'urchman0000@gmail.com', subject: "You got a new order!", delivery_method_options: @delivery_options)
   end
 end
