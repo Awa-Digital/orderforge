4d30336a0826b6c6acf45470ceb8b9536a3333fb
Uchenna Mba <urchymanny@gmail.com>
2023-01-30 00:48:26 +0000

    feat(image): change user image host to spaces

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-
-  include Cloudinary::CarrierWave
+  include CarrierWave::RMagick
+  # include Cloudinary::CarrierWave
 
   def public_id
     "jazzy/#{ENV['MEDIA_SUBFOLDER']}/users/user_#{model.id}_#{DateTime.now.to_i}"
@@ -8,7 +8,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [300, 300]
 
-  def default_url(*args)
-    "https://res.cloudinary.com/awadigital/image/upload/v1644139435/yaaaga/default_png.png"
+  def default_url(*_args)
+    'https://awa-apps.fra1.cdn.digitaloceanspaces.com/JJB/production/default_png.png'
   end
 end
