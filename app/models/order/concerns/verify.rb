592bf6fbacc437e331a353db45fc3139b717598a
Mba Uchenna <urchymanny@gmail.com>
2023-12-06 18:51:22 +0000

    Update verify.rb

--- app/models/order/concerns/verify.rb
+++ app/models/order/concerns/verify.rb
@@ -2,11 +2,13 @@ module Order::Concerns
   # verification of payment
   module Verify
     def verify
-      paid = payment.verify
+      return shout('Transaction is verified') if paid == true
+      
+      payment_status = payment.verify
     rescue StandardError
       raise "An error occurred while verifying this payment: #{payment.reference}"
     else
-      return shout('Transaction not paid') unless paid == true
+      return shout('Transaction not paid') unless payment_status == true
 
       complete_payment(payment)
     end
