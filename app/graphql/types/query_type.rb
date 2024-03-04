d14fd2c065922156f9af6863521f673e2939bfed
Mba Uchenna <urchymanny@gmail.com>
2024-03-04 14:14:42 +0000

    feat(graphql): basic sorting and filtering

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -16,14 +16,64 @@ module Types
     # loop through array names and use 'define_method(name)'
     model_names.each do |name|
       field_name = "Types::#{name.classify}Type"
-      field name.to_sym, field_name.constantize.connection_type, null: false
+
+      ### Sorting class generation
+      sort_name = "#{name.classify}SortEnum"
+      sort_fields = name.classify.constantize.column_names
+
+      # Dynamically create the enum class
+      enum_class = Class.new(GraphQL::Schema::Enum) do
+        # Dynamically add enum values based on the sort_fields array
+        sort_fields.each do |field|
+          value "#{field.upcase}_ASC", "Sort by #{field} in ascending order"
+          value "#{field.upcase}_DESC", "Sort by #{field} in descending order"
+        end
+      end
+
+      # insert new class into Types Module
+      Types.const_set(sort_name, enum_class)
+      # --------------
+
+      ### Filter input type generation
+      filter_input_name = "#{name.classify}FilterInput"
+      model_class = name.classify.constantize
+      filter_fields = model_class.columns.inject({}) do |hash, column|
+        hash[column.name.to_sym] = String if column.type == :string
+        hash
+      end
+
+      filter_input_class = Class.new(GraphQL::Schema::InputObject) do
+        filter_fields.each do |field_name, field_type|
+          argument field_name, field_type, required: false, description: "Filter by #{field_name}"
+        end
+      end
+
+      # Insert the new class into the Types module
+      Types.const_set(filter_input_name, filter_input_class)
+      # --------------
+
+
+      field name.to_sym, field_name.constantize.connection_type, null: false do
+        argument :sort, "Types::#{sort_name}".constantize, required: false
+        argument :filter, "Types::#{filter_input_name}".constantize, required: false, as: :filter_conditions
+      end
 
       field name.singularize.to_sym, field_name.constantize, null: false do
         argument :id, ID
       end
 
-      define_method(name) do
-        name.classify.constantize.all
+      define_method(name) do |sort: nil, filter_conditions: {}|
+        # Start with all records
+        records = name.classify.constantize.all
+
+        # Apply filtering logic based on `filter_conditions`
+        filter_conditions&.each do |key, value|
+          records = records.where(key => value) if value.present?
+        end
+
+        # Apply sorting logic
+
+        records
       end
 
       define_method(name.singularize) do |id:|
