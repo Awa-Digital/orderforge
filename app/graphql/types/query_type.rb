1964af2ba059977ecd8d5428428708c0227d21ea
Mba Uchenna <urchymanny@gmail.com>
2024-03-04 12:37:49 +0000

    feat(graphql): enable pagination

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,11 +13,10 @@ module Types
     end
 
     model_names = model_names.map { |s| s.gsub(/([a-z])([A-Z])/, '\1_\2').downcase.pluralize }
-
     # loop through array names and use 'define_method(name)'
     model_names.each do |name|
       field_name = "Types::#{name.classify}Type"
-      field name.to_sym, [field_name.constantize], null: false
+      field name.to_sym, [field_name.constantize.connection_type], null: false
 
       field name.singularize.to_sym, field_name.constantize, null: false do
         argument :id, ID
@@ -30,32 +29,6 @@ module Types
       define_method(name.singularize) do |id:|
         name.classify.constantize.find(id)
       end
-    end
-
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
-    end
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
-    end
-
-    field :nodes, [Types::NodeType, { null: true }], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
     end
   end
 end
