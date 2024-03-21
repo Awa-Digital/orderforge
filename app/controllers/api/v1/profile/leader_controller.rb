c0213b92cf3953526b15c2703961132b53ecf264
Mba Uchenna <urchymanny@gmail.com>
2024-03-21 12:44:46 +0000

    feat(leaderboard): revitalize

--- app/controllers/api/v1/profile/leader_controller.rb
+++ app/controllers/api/v1/profile/leader_controller.rb
@@ -3,25 +3,30 @@ class Api::V1::Profile::LeaderController < Api::V1::Profile::BaseController
   skip_before_action :authenticate_user
   before_action :authenticate_guest
   def board
-    @users = User.includes(:orders).order(spend_score: :desc).limit(10)
+    @users = User.order(spend_score: :desc).limit(10)
     data = make_data(@users)
     success({ message: 'Leaderboard fetched successfully', data: })
   end
 
   def make_data(users)
     arr = []
-    rank = 0
     users.each do |user|
       total_amount = user.orders.select(&:paid).sum(&:total)
       arr << {
         image: user.avatar,
         name: user.first_name.camelize,
         amount: total_amount,
-        rank: rank + 1
       }
-      rank += 1
     end
-    arr.sort_by { |user| user[:rank] }
+
+    arr.sort_by { |user| user[:amount] }.each_with_index do |user, index|
+      user[:rank] = index + 1
+    end
+
+    arr
   end
 
 end
+
+
+arr << { image: user.avatar, name: u.first_name.camelize, amount: total_amount, rank: rank + 1 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
+  extend FriendlyId
+  friendly_id :first_name, use: :slugged
+
   has_many :orders, dependent: :destroy
   has_many :payments, dependent: :destroy
   has_one :favourite, dependent: :destroy
