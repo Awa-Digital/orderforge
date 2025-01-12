394d79fa486e8fde717c2dff5b8f28f3205852ce
Uchenna Mba <uchenna.mba@accounto.io>
2025-01-12 04:25:32 +0000

    use sub domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :authenticate_user
   skip_before_action :authenticate_user, if: -> { request.path.start_with?('/admin') }
+  before_action :redirect_to_admin_path_if_needed
 
   include Responses
   include Authentication
 
+  private
+
+  def redirect_to_admin_path_if_needed
+    return unless request.subdomain == 'admin' && !request.path.start_with?('/admin')
+
+    redirect_to "/admin#{request.fullpath}", allow_other_host: true
+  end
 end
