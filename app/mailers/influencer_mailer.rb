2de179037fb8c6d6c180a163d5cc144a75800044
Uchenna Mba <uchenna@liftdsp.com>
2025-02-28 13:17:08 +0000

    influencer emails

--- app/mailers/influencer_mailer.rb
+++ app/mailers/influencer_mailer.rb
@@ -6,7 +6,12 @@ class InfluencerMailer < ApplicationMailer
     @user = Influencer.find(params[:id])
     @preheader = "Next Steps to get your affiliate account approved -"
 
-    mail(to: @user.email, from: "Jazzy's Burger <#{@delivery_options[:user_name]}>", subject: "Jazzy's Burger Affiliate Approval", delivery_method_options: @delivery_options)
+    mail(
+      to: @user.email,
+      from: "Jazzy's Burger <#{@delivery_options[:user_name]}>",
+      subject: "Next Steps to get your affiliate account approved",
+      delivery_method_options: @delivery_options
+    )
   rescue Net::SMTPAuthenticationError => e
     Sentry.capture_exception(Net::SMTPAuthenticationError.new(e))
     Sentry.capture_message(e)
@@ -16,7 +21,12 @@ class InfluencerMailer < ApplicationMailer
     @user = Influencer.find(params[:id])
     @preheader = "Your account has been approved -"
 
-    mail(to: @user.email, from: "Jazzy's Burger <#{@delivery_options[:user_name]}>", subject: "Jazzy's Burger:Affiliate Account Approved", delivery_method_options: @delivery_options)
+    mail(
+      to: @user.email,
+      from: "Jazzy's Burger <#{@delivery_options[:user_name]}>",
+      subject: "Affiliate Account Approved",
+      delivery_method_options: @delivery_options
+    )
   rescue Net::SMTPAuthenticationError => e
     Sentry.capture_exception(Net::SMTPAuthenticationError.new(e))
     Sentry.capture_message(e)
