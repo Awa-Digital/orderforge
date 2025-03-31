38b36f0b5887c34ee4abb36180a6e6fbd8a91dcc
Uchenna Mba <uchenna.mba@accounto.io>
2025-03-31 16:25:40 +0100

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # config/initializers/assets.rb
 
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w[application.js application.css]
+Rails.application.config.assets.precompile += %w[application.js application.css active_admin.js]
 Rails.application.config.assets.precompile << "bootstrap.min.js"
