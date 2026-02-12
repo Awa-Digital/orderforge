bac7ae02eef139836326a9afdf6ea88730777651
Master Uchay <uchenna@liftdsp.com>
2026-02-12 21:28:52 +0000

    fix influencer

--- app/admin/influencers.rb
+++ app/admin/influencers.rb
@@ -159,4 +159,10 @@ ActiveAdmin.register Influencer do
     end
     f.actions
   end
+
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
 end
