2831f6f20d87c7803c42097efc1d52bf2ecadaec
Master Uchay <uchenna@liftdsp.com>
2026-04-22 12:18:52 +0100

    disable rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,22 +2,24 @@
 
 # Rack::Attack.throttle('ip limit', limit: 50, period: 5.seconds, &:ip)
 
-bad_ips = ENV.fetch("BLOCKED_IPS", "0.0.0.1").split(',')
+# Reset the Rack::Attack ban with Rack::Attack.cache.reset!
 
-Rack::Attack.blocklist 'Block IPs from Environment Variable' do |req|
-  bad_ips.include?(req.ip)
-end
+# bad_ips = ENV.fetch("BLOCKED_IPS", "0.0.0.1").split(',')
 
-Rack::Attack.blocklist('allow2ban scrapers') do |req|
-  Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 40, findtime: 30.seconds, bantime: 10.minutes) do
-    true
-  end
-end
+# Rack::Attack.blocklist 'Block IPs from Environment Variable' do |req|
+#   bad_ips.include?(req.ip)
+# end
 
-ActiveSupport::Notifications.subscribe('throttle.rack_attack') do |_name, _start, _finish, _request_id, req|
-  Rails.logger.error("----- ----- ----- ----- ----- Rack::Attack::Throttle Too many Throttling Requests from IP: #{req[:request].ip}")
-end
+# Rack::Attack.blocklist('allow2ban scrapers') do |req|
+#   Rack::Attack::Allow2Ban.filter(req.ip, maxretry: 40, findtime: 30.seconds, bantime: 10.minutes) do
+#     true
+#   end
+# end
 
-ActiveSupport::Notifications.subscribe('blocklist.rack_attack') do |_name, _start, _finish, _request_id, req|
-  Rails.logger.error("----- ----- ----- ----- ----- Rack::Attack::Ban IP: #{req[:request].ip} has been Banned!")
-end
+# ActiveSupport::Notifications.subscribe('throttle.rack_attack') do |_name, _start, _finish, _request_id, req|
+#   Rails.logger.error("----- ----- ----- ----- ----- Rack::Attack::Throttle Too many Throttling Requests from IP: #{req[:request].ip}")
+# end
+
+# ActiveSupport::Notifications.subscribe('blocklist.rack_attack') do |_name, _start, _finish, _request_id, req|
+#   Rails.logger.error("----- ----- ----- ----- ----- Rack::Attack::Ban IP: #{req[:request].ip} has been Banned!")
+# end
